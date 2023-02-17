e05205482c0a7bd0f0064781542d5be3135f1a7c
guigattidev <gattiwhoomps@hotmail.com>
2023-02-17 10:29:05 -0300

    reduce

---
+++ 5-reduce.js
@@ -0,0 +1,18 @@
+// reduce()
+// array.reduce() leva dois parâmetros. O primeiro é o acumulador
+// e o segundo é o valor inicial que podemos definir.
+
+// Syntax:
+// reduce((accumulator, currentValue, currentIndex, array))
+
+// Example:
+const arr = [1, 2, 3, 4, 5];
+
+let result = array.reduce(function (accumulator, currentValue) {
+  return acc + val;
+}, 0);
+
+console.log(result);
+
+// Output:
+// 15
