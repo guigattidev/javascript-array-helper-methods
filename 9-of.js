d0f53e3193612b10b1e90641c41a259029736451
guigattidev <gattiwhoomps@hotmail.com>
2023-02-17 11:33:33 -0300

    of

---
+++ 9-of.js
@@ -0,0 +1,19 @@
+// of()
+// array.of() o método sempre cria um array que contém os valores
+// que você passa para ela, independentemente dos tipos ou do número
+// de argumentos. A principal diferença entre os construtores
+// array.of() e array() é que quando passamos um valor inteiro como
+// parâmetro em array.of(), ele cria um array com um único elemento
+// que passamos enquanto no construtor Array() ele cria um comprimento
+// de um determinado tamanho que é passado como parâmetro.
+
+// Syntax:
+// array.of(element0[, element1[, ...[, elementN]]]);
+
+// Example:
+let numbers = new Array(2);
+
+console.log(numbers.length);
+
+// Output:
+// 2
