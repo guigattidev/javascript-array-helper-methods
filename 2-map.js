a023e8233d04cf670180035fdb564b28999829e5
guigattidev <gattiwhoomps@hotmail.com>
2023-02-17 09:47:55 -0300

    map

---
+++ 2-map.js
@@ -0,0 +1,20 @@
+// map()
+// array.map() percorre cada item do array, do mesmo jeito que
+// forEach() faz mas map() chama a função para cada elemento do
+// array em uma ordem particular
+
+// Syntax:
+// array.map(function(currentValue, index, arr));
+
+// Example:
+const arr = [
+  { name: "James", age: 18 },
+  { name: "Alice", age: 20 },
+];
+
+const personName = arr.map((person) => person.name);
+
+console.log(personName);
+
+// Ouput:
+// [“James”,”Alice”]
